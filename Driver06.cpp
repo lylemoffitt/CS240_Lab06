3ec6d5e40cb422dec12aa7f5add92451fabb646b
TaniaNsouli <tinsouli@syr.edu>
2013-07-25 14:08:46 -0400

    Update Driver06.cpp

--- Driver06.cpp
+++ Driver06.cpp
@@ -1,38 +1,46 @@
-
 #include "Lab06.h"
-
 using namespace std;
 
 int main()
 {
     data Hello;
-    ifstream file("imdbTest.txt");
-    Hello.readFile(file);
+	ifstream file("imdbTest.txt");
+	Hello.readFile(file);
+	Hello.Display();
+	srand(time(0));
+	/*
+	string str1 = "ABCD", str2 = "EFGH";
+	cout << "str1: " << str1 << endl << "str2: " << str2 << endl;
+	cout << "str1.compare(str2): " << str1.compare(str2) << endl;
+	cout << "str2.compare(str1): " << str2.compare(str1) << endl;
 
-    /*
-        string item, line1;
-        ifstream infile("imdbTest.txt");
-        getline(infile, item, ',');
-        //one problem it reads in the space
-        getline(infile, line1, ',');
-        line1.erase(0,1); //we'll have to ask if there will always be a space after each comma
+	/*
+		string item, line1;
+		ifstream infile("imdbTest.txt");
+		getline(infile, item, ',');
+		//one problem it reads in the space
+		getline(infile, line1, ',');
+		line1.erase(0,1); //we'll have to ask if there will always be a space after each comma
+		
+		int* hi;
+		hi = nullptr;
 
-        int* hi;
-        hi = nullptr;
+		cout << item << endl << line1 << endl;
 
-        cout << item << endl << line1 << endl;
+		string* strArray;
+		string str = "This is the day", temp;
 
-        string* strArray;
-        string str = "This is the day", temp;
+		int i=0;
+		/*
+		while(i<str.length())
+		{
+			//grrr
+		}
 
-        int i=0;
-        /*
-        while(i<str.length())
-        {
-            //grrr
-        }
-
-        */
-    return 0;
+		*/
+	return 0;
 }
+
+
 //Will there always be a space after each comma?
+//
